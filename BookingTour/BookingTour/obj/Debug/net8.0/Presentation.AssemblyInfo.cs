bb20a6697a18f05b5bf9fc532323c5c78be02504
NguyenMinhKiet <nguyenminhkiet642002@gmail.com>
2024-11-30 03:30:17 +0700

    update img

--- BookingTour/BookingTour/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ BookingTour/BookingTour/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb078e7112417f591d25f17ccfde6804bc71d66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+608b306c7219216eae79da50a3c4a59c60af4622")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
